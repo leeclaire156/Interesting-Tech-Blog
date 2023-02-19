0d9c76693d55a0042f9453a552aa97f9ae88d795
Claire <lee.claire156@gmail.com>
2023-02-19 01:22:05 -0500

    Console logs the username and password from login and signup forms

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+var loginBtn = document.querySelector(".login-button");
+
+loginBtn.addEventListener("click", logIn);
+
+async function logIn(event) {
+    event.preventDefault();
+    // Collect values from the login form
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        console.log(username, password);
+        // // Send a POST request to the API endpoint
+        // const response = await fetch('/api/users/login', {
+        //     method: 'POST',
+        //     body: JSON.stringify({ username, password }),
+        //     headers: { 'Content-Type': 'application/json' },
+        // });
+
+        // if (response.ok) {
+        //     // If successful, redirect the browser to the homepage
+        //     document.location.replace('/homepage');
+        // } else {
+        //     alert(response.statusText);
+        // }
+    }
+};
