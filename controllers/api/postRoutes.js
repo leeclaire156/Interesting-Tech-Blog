d1249b9cd5a93a89510ec0e8f6c6c34b84986fa1
Claire <lee.claire156@gmail.com>
2023-02-19 21:22:54 -0500

    Creates rudimentary POST route for blog posts

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+//Creates (through POST route) new post from sign up form
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create(req.body);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
