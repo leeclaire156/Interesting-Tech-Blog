a6d39c33003917c6a9ee10a4c2c7d5a6122e19d4
lee.claire156 <lee.claire156@gmail.com>
2023-02-22 02:10:31 -0500

    Allows user to view single post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -40,6 +40,29 @@ router.get('/', async (req, res) => {
 }
 );
 
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        // res.status(200).json(postData);
+
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.get('/login', (req, res) => {
     //Once the user is logged in, the user should be redirected to the homepage
     if (req.session.loggedIn) {
