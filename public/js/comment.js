7d82b518b6bfed2a7ad3957c92b1409d024bd21d
Claire <lee.claire156@gmail.com>
2023-02-25 18:11:41 -0500

    Deletes comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,3 +1,4 @@
+// Add new comments functions
 var addCommentBtn = document.querySelector(".add-comment-button");
 
 if (addCommentBtn) {
@@ -48,3 +49,98 @@ function hideCommentForm(event) {
     addCommentForm.classList.add("d-none");
     commentFormBtn.classList.remove("d-none");
 };
+
+
+// Delete comment functions
+
+var deleteBtns = document.querySelectorAll(".delete-comment-button");
+
+deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener('click', deleteComment);
+})
+
+async function deleteComment() {
+    const id = this.getAttribute('data-id');
+
+    const postTitle = document.querySelector(".post-title");
+    const post_id = postTitle.getAttribute("data-id");
+
+    console.log("Clicked!", id)
+    const response = await fetch(`/api/comments/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        document.location.replace(`/post/${post_id}`);
+    } else {
+        alert(response.statusText);
+        alert('Failed to delete comment');
+    }
+};
+
+// // Edit comments functions
+
+// var editPostForm = document.querySelector("#edit-post-form");
+// var newPostBtn = document.querySelector(".new-post-button");
+// var goBackBtn = document.querySelector(".back-button");
+
+// var editBtns = document.querySelectorAll(".edit-post-button");
+// var updateBtn = document.querySelector(".update-button")
+
+// editBtns.forEach((editBtn) => {
+//     editBtn.addEventListener('click', openPostEditor);
+// })
+
+// function openPostEditor(event) {
+//     event.preventDefault();
+
+//     // Obtains information about target post
+//     var id = this.getAttribute('data-id');
+//     var titleText = this.parentElement.parentElement.previousElementSibling.children[0].innerText;
+//     var bodyContent = this.parentElement.previousElementSibling.textContent.trim();
+
+//     // Opens edit form
+//     editPostForm.classList.remove("d-none");
+//     newPostBtn.classList.add("d-none");
+
+//     // Sets edit form's data-id to the respective post's id for the Update button
+//     const editFormID = document.createAttribute("data-id");
+//     editFormID.value = id;
+//     const updateBtn = document.querySelector('.update-button');
+//     updateBtn.setAttributeNode(editFormID);
+
+//     // Sets edit form's title to the respective post's title
+//     const att = document.createAttribute("value");
+//     att.value = titleText;
+//     const editPostFormTitle = document.querySelector('#edited-post-title');
+//     editPostFormTitle.setAttributeNode(att);
+
+//     // Sets edit form's body to the respective post's body
+//     const editPostFormBody = document.querySelector('#edited-post-content');
+//     editPostFormBody.innerText = bodyContent;
+// }
+
+// updateBtn.addEventListener('click', updatePost);
+// async function updatePost(event) {
+//     event.preventDefault();
+
+//     const id = this.getAttribute('data-id');
+
+//     const title = document.querySelector('#edited-post-title').value;
+
+//     const body = document.querySelector('#edited-post-content').value;
+
+//     const response = await fetch(`/api/posts/${id}`, {
+//         method: 'PUT',
+//         body: JSON.stringify({ title, body }),
+//         headers: { 'Content-Type': 'application/json' },
+//     });
+
+
+//     if (response.ok) {
+//         document.location.replace('/dashboard');
+//     } else {
+//         alert(response.statusText);
+//         alert('Failed to edit post');
+//     }
+// };
